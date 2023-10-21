7825cd118c6edd50c5717dec5620e7acbe6c8e26
leandrosardi <leandro.sardi@expandedventure.com>
2023-10-20 21:12:04 -0300

    scrape command

--- cli/scrape.rb
+++ cli/scrape.rb
@@ -1,42 +1,51 @@
-require 'bots'
+#require 'bots'
+require_relative '../../../bots/lib/bots.rb'
 
-l = BlackStack::LocalLogger.new('./indeed1.log')
+require 'pry'
+require 'simple_command_line_parser'
+require 'simple_cloud_logging'
+require 'blackstack-core'
+require 'colorize'
+require 'csv'
+require_relative '../config.rb'
 
-puts "\n\nEXAMPLE 1".blue
-puts "Find domain of a company using Google Search API.\nCount the number of 'not-found' and 'error' cases.\n".yellow
+# 
+parser = BlackStack::SimpleCommandLineParser.new(
+  :description => 'This command upload a list of companies to FreeLeadsData for enrichment.', 
+  :configuration => [{
+    :name=>'tag', 
+    :mandatory=>true, 
+    :description=>'Label the searches we are running. Mandatory.', 
+    :type=>BlackStack::SimpleCommandLineParser::STRING,
+  }]
+)
 
-l.logs 'initialize GoogleBot... '
-bot = BlackStack::Bots::Indeed.new(proxy: nil)
-l.done
+l = BlackStack::LocalLogger.new('scrape.log')
 
-a = [
-    {
-        output_filename: 'indeed03',
-        search: "https://www.indeed.com/jobs?q=full+time&l=Medford%2C+OR&radius=35&vjk=5f70c02d46c70c65",
-    }, {
-        output_filename: 'indeed04',
-        search: "https://www.indeed.com/q-full-time-$50,000-l-Charleston,-SC-jobs.html?vjk=d66cee732eb0d03b",
-    }, {
-        output_filename: 'indeed05',
-        search: "https://www.indeed.com/jobs?q=full+time+%2450%2C000&l=Raleigh-Durham%2C+NC&vjk=67ca1a2862f9fa28",
-    }, {
-        output_filename: 'indeed06',
-        search: "https://www.indeed.com/jobs?q=full+time+%2450%2C000&l=Reno%2C+NV&vjk=bbe66fed429fcf4b",
-    },
-] 
+l.log 'INDEED SCRAPER'.yellow
 
-a.each { |h|
-    output_filename = h[:output_filename]
-    search = h[:search]
+l.logs 'Tag: ' 
+tag = parser.value('tag').to_s
+l.logf tag.blue
+
+l.logs 'initialize IndeedBot... '
+bot = BlackStack::Bots::Indeed.new(nil)
+l.logf 'done'.green
+
+# load urls from ../urls/#{tag}.txt
+l.logs 'Load URLs... '
+a = File.readlines("../urls/#{tag}.txt").map { |line| line.strip }
+l.logf 'done'.green + " (#{a.length.to_s.blue} URLs)"
+
+a.each { |s|
+    output_filename = "../csv/#{tag}.csv"
+    search = s
     start = 0
     while start <= 640
         l.logs "start=#{start}... "
         begin
             url = "#{search}&start=#{start}"
             ret = bot.results(url)
-            # save ret into a json file
-            #File.open("./#{output_filename}.start-#{start.to_s}.json", 'w') { |f| f.write(ret.to_json) }
-            # save into a CSV file
             CSV.open("./#{output_filename}.all.csv", 'a+b') { |csv|
                 csv << ['title', 'url', 'company', 'location', 'salary', 'posted', 'snippets']
                 ret.each { |h|
